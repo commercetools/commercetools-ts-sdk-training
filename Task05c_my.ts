923caa8b9ace8424ee4a8bdaee2f707881411a03
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2022-06-24 10:56:23 +0200

    Leave one function to implement

--- Task05c_my.ts
+++ Task05c_my.ts
@@ -17,25 +17,25 @@ const storeCustomerEmail = "test-local@test.com";
 
 getMe().then(log).catch(log);
 
-getMyOrders()
-    .then(orders =>
-        orders.body.results.forEach(order =>
-            log(order.id + " : " + order.totalPrice.centAmount)
-        )
-    )
-    .catch(log);
+// getMyOrders()
+//     .then(orders =>
+//         orders.body.results.forEach(order =>
+//             log(order.id + " : " + order.totalPrice.centAmount)
+//         )
+//     )
+//     .catch(log);
 
-createMyCart(customerEmail).then(log).catch(log);
+// createMyCart(customerEmail).then(log).catch(log);
 
-getMyActiveCart().then(log).catch(log);
+// getMyActiveCart().then(log).catch(log);
 
 // TODO: Store SPA api-client
 
-getStoreMe().then(log).catch(log);
+// getStoreMe().then(log).catch(log);
 
-getStoreMyOrders().then(log).catch(log);
+// getStoreMyOrders().then(log).catch(log);
 
-createInStoreMyCart(storeCustomerEmail).then(log).catch(log);
+// createInStoreMyCart(storeCustomerEmail).then(log).catch(log);
 
-getStoreMyActiveCart().then(log).catch(log);
+// getStoreMyActiveCart().then(log).catch(log);
 
