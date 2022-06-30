b883168e1743ed640aface1a420fe188cded2d63
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2022-06-30 16:26:09 +0200

    Fix typo

--- handson/importService.ts
+++ handson/importService.ts
@@ -55,7 +55,7 @@ const createProductDraftImportRequest = async (): Promise<ProductDraftImportRequ
 
 const getProductDraftImportArray = async (): Promise<Array<ProductDraftImport>> => {
 
-    const participantNamePrefix = "sfs";
+    const participantNamePrefix = "tt";
 
     // Get products data from csv
     const products = await csvtojsonV2()
@@ -81,8 +81,8 @@ const getProductDraftImportArray = async (): Promise<Array<ProductDraftImport>> 
                 "de": participantNamePrefix + "-" + product.productName
             },
             description: {
-                "en": product.description,
-                "de": product.description
+                "en": product.productDescription,
+                "de": product.productDescription
             },
             masterVariant: {
                 sku: participantNamePrefix + "-" + product.inventoryId,
