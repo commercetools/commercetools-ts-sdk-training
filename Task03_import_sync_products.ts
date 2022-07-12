ad794e4f79a6e15968dd418c8ce4280a1f394edb
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2022-07-12 10:44:52 +0200

    Use the word with what import API refers to this operation

--- Task03_import_sync_products.ts
+++ Task03_import_sync_products.ts
@@ -13,7 +13,7 @@ const containerKey = "tt-ImportContainer";
 createImportContainer(containerKey).then(log).catch(log);
 
 // import products
-// importProducts(containerKey).then(log).catch(log);
+// importProductDrafts(containerKey).then(log).catch(log);
 
 // check import summary for your container
 // checkImportSummary(containerKey).then(log).catch(log);
