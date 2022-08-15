0b5a7509d52e95171afa5afd8d1c6cd21a9c5cbb
Tuag <tuag@imaginationmedia.com>
2022-08-15 12:27:28 -0300

    TASK-2 IML-125 add customer group to customer

--- Task02_customers.ts
+++ Task02_customers.ts
@@ -29,12 +29,12 @@ const customerDraft: CustomerDraft = {
 
 // getCustomerByKey(customerDraft.key!).then(log).catch(log);
 
-getCustomerByKey(customerDraft.key!)
-  .then(createCustomerToken)
-  .then(confirmCustomerEmail)
+// getCustomerByKey(customerDraft.key!)
+//   .then(createCustomerToken)
+//   .then(confirmCustomerEmail)
+//   .then(log)
+//   .catch(log);
+
+assignCustomerToCustomerGroup(customerDraft.key!, "indoor-customers")
   .then(log)
   .catch(log);
-
-// assignCustomerToCustomerGroup(customerDraft.key!, "indoor-customers")
-//     .then(log)
-//     .catch(log);
