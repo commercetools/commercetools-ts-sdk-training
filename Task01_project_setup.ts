0f102a0d671079dfe3e5972b80bb6856b0fce9a8
Tuag <tuag@imaginationmedia.com>
2022-08-15 11:14:04 -0300

    get item using key

--- Task01_project_setup.ts
+++ Task01_project_setup.ts
@@ -11,14 +11,20 @@ import { apiRoot } from "./handson/client";
 // apiRoot.get().execute().then(log).catch(log);
 
 // TODO: Get shipping method by id
-apiRoot
-  .shippingMethods()
-  .withId({
-    ID: "42241db8-973e-4335-a24a-612c30185444",
-  })
-  .get()
-  .execute()
-  .then(log)
-  .catch(log);
+// apiRoot
+//   .shippingMethods()
+//   .withId({
+//     ID: "42241db8-973e-4335-a24a-612c30185444",
+//   })
+//   .get()
+//   .execute()
+//   .then(log)
+//   .catch(log);
 
 // TODO: Get standard tax category by key
+apiRoot
+  .taxCategories()
+  .withKey({ key: "standard-tax-category" })
+  .get()
+  .execute()
+  .then(log);
