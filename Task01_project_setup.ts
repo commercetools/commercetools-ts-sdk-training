bc4468f24bdb18290939bc6b81eb24a76ee0819a
Tuag <tuag@imaginationmedia.com>
2022-08-15 11:10:23 -0300

    task-1 getting shipping method by id

--- Task01_project_setup.ts
+++ Task01_project_setup.ts
@@ -8,8 +8,17 @@ import { apiRoot } from "./handson/client";
 // https://docs.commercetools.com/http-api-projects-project.html#get-project
 
 // TODO: Get project settings
-apiRoot.get().execute().then(log).catch(log);
+// apiRoot.get().execute().then(log).catch(log);
 
 // TODO: Get shipping method by id
+apiRoot
+  .shippingMethods()
+  .withId({
+    ID: "42241db8-973e-4335-a24a-612c30185444",
+  })
+  .get()
+  .execute()
+  .then(log)
+  .catch(log);
 
 // TODO: Get standard tax category by key
