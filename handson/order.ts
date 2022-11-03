0965faba2e29d16167ff668edcae92bf2d2404b9
Nagesh Dixit <74191400+nagesh-dixit@users.noreply.github.com>
2022-11-03 16:51:02 +0100

    updated sdk to 4.0.0 and added quotes exercise.

--- handson/order.ts
+++ handson/order.ts
@@ -1,6 +1,7 @@
-import { ClientResponse, Cart, CustomerSignin, CustomerSignInResult, Order, OrderFromCartDraft, OrderState } from "@commercetools/platform-sdk";
+import { ClientResponse, Cart, CustomerSignin, CustomerSignInResult, Order, OrderFromCartDraft, OrderState, OrderFromQuoteDraft } from "@commercetools/platform-sdk";
 import { apiRoot } from "./client";
 import { getCustomerByKey } from "./customer";
+import { getQuoteById, getQuoteRequestById } from "./quotes";
 
 export const createCart = (customerKey: string): Promise<ClientResponse<Cart>> => {
     return getCustomerByKey(customerKey)
@@ -83,6 +84,34 @@ export const addDiscountCodeToCart = (cartId: string, discountCode: string): Pro
         );
 }
 
+export const addDirectDiscountToCart = (cartId: string, percent: number): Promise<ClientResponse<Cart>> =>
+    getCartById(cartId)
+        .then(cart => apiRoot
+            .carts()
+            .withId({ ID: cartId })
+            .post({
+                body: {
+                    version: cart.body.version,
+                    actions: [{
+                        action: "setDirectDiscounts",
+                        discounts: [
+                            {
+                                value: {
+                                    type: "relative",
+                                    permyriad: percent * 100,
+                                },
+                                target: {
+                                    type: "lineItems",
+                                    predicate: "1 = 1"
+                                }
+                            }
+                        ]
+                    }]
+                }
+            })
+            .execute()
+        );
+
 export const recalculate = (cartId: string): Promise<ClientResponse<Cart>> =>
     getCartById(cartId).then(cart =>
         apiRoot
@@ -151,6 +180,28 @@ const createOrderFromCartDraft = (cartId: string): Promise<OrderFromCartDraft> =
                 typeId: "cart"
             },
             version: cart.body.version,
+        };
+    });
+
+export const createOrderFromQuote = (quoteId: string): Promise<ClientResponse<Order>> =>
+    createOrderFromQuoteDraft(quoteId).then(orderFromQuoteDraft =>
+        apiRoot
+            .orders()
+            .orderQuote()
+            .post({
+                body: orderFromQuoteDraft
+            })
+            .execute()
+    );
+
+const createOrderFromQuoteDraft = (quoteId: string): Promise<OrderFromQuoteDraft> =>
+    getQuoteById(quoteId).then(quote => {
+        return {
+            quote: {
+                id: quoteId,
+                typeId: "quote"
+            },
+            version: quote.body.version
         };
     });
 
---
+++ handson/quotes.ts
@@ -0,0 +1,108 @@
+import { QuoteRequest, ClientResponse, StagedQuote, StagedQuoteState, Quote } from "@commercetools/platform-sdk";
+import { getCartById } from "./order";
+import { apiRoot } from "./client";
+
+
+export const getQuoteRequestById = (ID: string):Promise<ClientResponse<QuoteRequest>> =>
+    apiRoot.quoteRequests()
+    .withId({ ID })
+    .get()
+    .execute()
+
+export const getStagedQuoteById = (ID: string):Promise<ClientResponse<StagedQuote>> =>
+    apiRoot.stagedQuotes()
+    .withId({ ID })
+    .get()
+    .execute()
+
+export const getQuoteById = (ID: string):Promise<ClientResponse<Quote>> =>
+    apiRoot.quotes()
+    .withId({ ID })
+    .get()
+    .execute()
+
+export const createQuoteRequest = (cartId: string): Promise<ClientResponse<QuoteRequest>> =>
+    getCartById(cartId).then(cart => 
+            apiRoot.quoteRequests()
+                .post({
+                    body: {
+                        cart: {id:cartId,typeId: "cart"},
+                        cartVersion: cart.body.version,
+                        comment: "I need 10% discount"
+                    }
+                })
+                .execute()
+        )
+
+export const createStagedQuote = (quoteRequestId: string): Promise<ClientResponse<StagedQuote>> =>
+    getQuoteRequestById(quoteRequestId).then(quoteRequest => 
+            apiRoot.stagedQuotes()
+                .post({
+                    body: {
+                        quoteRequest: {id:quoteRequestId,typeId:"quote-request"},
+                        quoteRequestVersion: quoteRequest.body.version
+                    }
+                })
+                .execute()
+        )
+
+export const setValidTo = (stagedQuoteId: string, days: number): Promise<ClientResponse<StagedQuote>> =>
+    getStagedQuoteById(stagedQuoteId).then(stagedQuote => 
+            apiRoot.stagedQuotes()
+                .withId({ID: stagedQuoteId})
+                .post({
+                    body: {
+                        actions: [{
+                            action: "setValidTo",
+                            validTo: new Date(Date.now() + days*1000*60*60*24).toISOString()
+                        }],
+                        version: stagedQuote.body.version
+                    }
+                })
+                .execute()
+        )
+
+export const setSellerComment = (stagedQuoteId: string, commment: string): Promise<ClientResponse<StagedQuote>> =>
+    getStagedQuoteById(stagedQuoteId).then(stagedQuote => 
+            apiRoot.stagedQuotes()
+                .withId({ID: stagedQuoteId})
+                .post({
+                    body: {
+                        actions: [{
+                            action: "setSellerComment",
+                            sellerComment:commment
+                        }],
+                        version: stagedQuote.body.version
+                    }
+                })
+                .execute()
+        )
+
+export const changeStagedQuoteState = (stagedQuoteId: string, state: StagedQuoteState): Promise<ClientResponse<StagedQuote>> =>
+    getStagedQuoteById(stagedQuoteId).then(stagedQuote => 
+            apiRoot.stagedQuotes()
+                .withId({ID: stagedQuoteId})
+                .post({
+                    body: {
+                        actions: [{
+                            action: "changeStagedQuoteState",
+                            stagedQuoteState: state
+                        }],
+                        version: stagedQuote.body.version
+                    }
+                })
+                .execute()
+        )
+
+
+export const createQuote = (stagedQuoteId: string): Promise<ClientResponse<Quote>> =>
+    getStagedQuoteById(stagedQuoteId).then(stagedQuote => 
+            apiRoot.quotes()
+                .post({
+                    body: {
+                        stagedQuote: {id:stagedQuoteId,typeId:"staged-quote"},
+                        stagedQuoteVersion: stagedQuote.body.version
+                    }
+                })
+                .execute()
+        )
