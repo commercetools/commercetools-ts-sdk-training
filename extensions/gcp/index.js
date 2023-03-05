104dc9a0247ae94c0347e1fe225d396bca83c6f3
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2023-03-05 18:23:28 +0100

    Change GCP Extension code to use node v16

--- extensions/gcp/index.js
+++ extensions/gcp/index.js
@@ -4,47 +4,46 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-const { projectKey, getCustomerById } = require("./client.js");
 
-exports.main = (req, res) => {
-    console.log("incoming event for project", projectKey);
+const functions = require('@google-cloud/functions-framework');
+const { projectKey, getCustomerById } = require("./client.js")
 
-    const order = req.body.resource?.obj;
+functions.http('main', (req, res) => {
+  console.log("NEW incoming event for project", projectKey);
 
-    console.log("new order has arrived");
-    console.log("order:" + JSON.stringify(order));
+  const order = req.body.resource?.obj;
 
-    const customerId = order?.customerId;
-    console.log("customerId:" + customerId);
+  console.log("new order has arrived");
+  console.log("order:" + JSON.stringify(order));
 
-    getCustomerById(customerId).then(customerFetched => {
-        console.log("customerFetched" + JSON.stringify(customerFetched));
+  const customerId = order?.customerId;
+  console.log("customerId:" + customerId);
 
-        const canPlaceOrders = customerFetched.body.custom?.fields?.["allowed-to-place-orders"];
+  getCustomerById(customerId).then(customerFetched => {
+    console.log("customerFetched" + JSON.stringify(customerFetched));
 
-        switch (canPlaceOrders) {
-            case undefined:
-            case true:
-                console.log("Can place order or custom field not defined");
-                res.status(200).end();
-                break;
-            case false:
-                console.error("Customer can not place orders");
-                const errors = [{
-                    code: "InvalidOperation",
-                    message: "Customer has been blocked from placing orders"
-                }]
-                res.status(400).send({ errors });
-                break;
-        }
-
-    }).catch(e => {
-        console.error("Customer was not found or general error");
+    const canPlaceOrders = customerFetched.body.custom?.fields?.["allowed-to-place-orders"];
+    switch (canPlaceOrders) {
+      case undefined:
+      case true:
+        console.log("Can place order or custom field not defined");
+        res.status(200).end();
+        break;
+      case false:
+        console.log("Customer can not place orders");
         const errors = [{
-            code: "InvalidOperation",
-            message: "Customer was not found"
+            code: "InvalidInput",
+            message: "Customer has been blocked from placing orders"
         }]
-        res.status(400).send({ errors });
+        res.status(400).json({ errors });
+        break;
+      }
+    }).catch(e => {
+      console.error("Customer was not found or general error" + e);
+      const errors = [{
+          code: "InvalidOperation",
+          message: e
+      }]
+      res.status(400).json({ errors });
     });
-
-};
+});
