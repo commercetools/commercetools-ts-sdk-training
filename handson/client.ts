cd0761faf3af9d683390d02cb9eb65500249234f
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2024-01-03 15:54:46 +0100

    Update dependencies, upgrade commercetools packages, remove node-fetch dependency, update node version

--- handson/client.ts
+++ handson/client.ts
@@ -1,27 +1,29 @@
 import { createApiBuilderFromCtpClient as createImportApiBuilderFromCtpClient } from "@commercetools/importapi-sdk";
 import { createApiBuilderFromCtpClient } from "@commercetools/platform-sdk";
-import { AuthMiddlewareOptions, ClientBuilder, HttpMiddlewareOptions, PasswordAuthMiddlewareOptions } from "@commercetools/sdk-client-v2";
-import fetch from "node-fetch";
+import {
+    AuthMiddlewareOptions,
+    ClientBuilder,
+    HttpMiddlewareOptions,
+    PasswordAuthMiddlewareOptions,
+} from "@commercetools/sdk-client-v2";
 import { ApiRoot, ImportApiRoot } from "../types/global";
 import { Prefix, Config, readConfig } from "../utils/config";
 
-
 const createApiClient = () => {
     throw new Error("Function not implemented");
-}
+};
 
 const createImportApiClient = () => {
     throw new Error("Function not implemented");
-}
+};
 
 const createStoreApiClient = () => {
     throw new Error("Function not implemented");
-}
+};
 
 const createMyApiClient = () => {
     throw new Error("Function not implemented");
-}
-
+};
 
 export const apiRoot: ApiRoot = createApiClient();
 // export const importApiRoot: ImportApiRoot = createImportApiClient();
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,4 @@
-import type { default as FetchFunc } from "node-fetch";
-
-declare global {
-    const fetch: typeof FetchFunc;
-}
-
-declare module "chalk-animation"
+declare module "chalk-animation";
 
 export type { ByProjectKeyRequestBuilder as ApiRoot } from "@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder";
 export type { ByProjectKeyRequestBuilder as ImportApiRoot } from "@commercetools/importapi-sdk/dist/declarations/src/generated/client/by-project-key-request-builder";
