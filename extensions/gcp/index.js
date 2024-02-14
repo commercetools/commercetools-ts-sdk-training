fac06b90b026376ade8e251da54623d4d38eb350
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2024-02-14 11:47:29 +0100

    Correct responseType, add statusCode, and improve error handling

--- extensions/gcp/index.js
+++ extensions/gcp/index.js
@@ -9,41 +9,41 @@ const functions = require('@google-cloud/functions-framework');
 const { projectKey, getCustomerById } = require("./client.js")
 
 functions.http('main', (req, res) => {
-  console.log("NEW incoming event for project", projectKey);
+  console.log("Incoming event for project", projectKey);
 
-  const order = req.body.resource?.obj;
+  const order = event?.resource?.obj;
 
-  console.log("new order has arrived");
-  console.log("order:" + JSON.stringify(order));
+  console.log("New order has arrived", order?.lastModifiedAt);
+  console.log("Order:", JSON.stringify(order));
 
   const customerId = order?.customerId;
-  console.log("customerId:" + customerId);
+  console.log("CustomerId:", customerId);
 
   getCustomerById(customerId).then(customerFetched => {
-    console.log("customerFetched" + JSON.stringify(customerFetched));
+    console.log("customerFetched", JSON.stringify(customerFetched));
 
-    const canPlaceOrders = customerFetched.body.custom?.fields?.["allowed-to-place-orders"];
+    const canPlaceOrders = customerFetched?.body?.custom?.fields?.["allowed-to-place-orders"];
     switch (canPlaceOrders) {
       case undefined:
       case true:
         console.log("Can place order or custom field not defined");
-        res.status(200).end();
+        res.status(201).end();
         break;
       case false:
         console.log("Customer can not place orders");
         const errors = [{
-            code: "InvalidInput",
-            message: "Customer has been blocked from placing orders"
+          code: "InvalidOperation",
+          message: "Customer has been blocked from placing orders"
         }]
         res.status(400).json({ errors });
         break;
-      }
-    }).catch(e => {
-      console.error("Customer was not found or general error" + e);
-      const errors = [{
-          code: "InvalidOperation",
-          message: e
-      }]
-      res.status(400).json({ errors });
-    });
+    }
+  }).catch(e => {
+    console.error("Customer was not found or general error", e);
+    const errors = [{
+      code: "InvalidOperation",
+      message: e.message || "An unexpected error occurred"
+    }]
+    res.status(400).json({ errors });
+  });
 });
