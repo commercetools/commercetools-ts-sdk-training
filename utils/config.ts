17cdbaf2564fa31159719926194d9080bfbc331c
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2024-11-25 11:49:22 +0100

    Remove me endpoint exercise (#28)

--- utils/config.ts
+++ utils/config.ts
@@ -3,26 +3,26 @@ import { config } from "dotenv";
 config();
 
 export const enum Prefix {
-    DEV = "DEV",
-    IMPORT = "IMPORT",
-    STORE = "STORE",
-    AWS = "AWS"
-};
+  DEV = "DEV",
+  IMPORT = "IMPORT",
+  STORE = "STORE",
+  AWS = "AWS",
+}
 
 export const readConfig = (prefix: string) => {
-    return {
-        clientId: process.env[prefix + "_CLIENT_ID"] || "",
-        clientSecret: process.env[prefix + "_CLIENT_SECRET"] || "",
-        projectKey: process.env[prefix + "_PROJECT_KEY"] || "",
-        oauthHost: process.env[prefix + "_AUTH_URL"] || "",
-        host: process.env[prefix + "_API_URL"] || ""
-    };
-}
+  return {
+    clientId: process.env[prefix + "_CLIENT_ID"] || "",
+    clientSecret: process.env[prefix + "_CLIENT_SECRET"] || "",
+    projectKey: process.env[prefix + "_PROJECT_KEY"] || "",
+    oauthHost: process.env[prefix + "_AUTH_URL"] || "",
+    host: process.env[prefix + "_API_URL"] || "",
+  };
+};
 
 export type Config = {
-    clientId: string;
-    clientSecret: string;
-    projectKey: string;
-    oauthHost: string;
-    host: string;
-}
+  clientId: string;
+  clientSecret: string;
+  projectKey: string;
+  oauthHost: string;
+  host: string;
+};
